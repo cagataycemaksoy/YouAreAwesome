5c16b7661d69412b5705012accee0a0750cb6601
cagataycemaksoy <cemaksoy7890@gmail.com>
2025-11-05 16:46:30 +0100

    new challenge completed

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -10,19 +10,21 @@ import SwiftUI
 struct ContentView: View {
   @State private var isAwesome = false
   private var color: Color {
-    isAwesome ? .orange : .indigo
+    isAwesome ? .orange : Color(red: 0.65, green: 0.1, blue: 0.1)
   }
 
     var body: some View {
         VStack(spacing: 20) {
           Spacer()
-          Image(systemName: isAwesome ? "sun.max.fill" : "hand.thumbsup")
+          Image(isAwesome ? "image0" : "image1")
             .resizable()
             .scaledToFit()
-            .foregroundStyle(color)
+            .clipShape(RoundedRectangle(cornerRadius: 25))
+            .shadow(radius: 25)
+            .padding()
           Text(isAwesome ? "You Are Awesome!" : "You Are Great!")
             .font(.title)
-            .fontWeight(.light)
+            .fontWeight(.bold)
             .foregroundStyle(color)
           Spacer()
           Button("Press Me!") {
