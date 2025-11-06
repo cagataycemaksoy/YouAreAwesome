ea65637a39b47b19744ece9c97c5b81140005572
cagataycemaksoy <cemaksoy7890@gmail.com>
2025-11-07 00:48:51 +0100

    audio playing added and code refactored

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -6,10 +6,15 @@
 //
 
 import SwiftUI
+import AVFAudio
 
 struct ContentView: View {
+  @State private var audioPlayer: AVAudioPlayer!
+  
   @State private var imageNum = Int.random(in: 0...9)
   @State private var textNum = Int.random(in: 0...5)
+  @State private var audioNum = Int.random(in: 0...5)
+  
   private let phrases = ["You Are Great!", "Great Energy!", "You Are Awesome!", "Such a kind person.", "You Are Fantastic!", "Fabulous! That is you."]
   
     var body: some View {
@@ -43,16 +48,33 @@ struct ContentView: View {
     }
   
   private func buttonPressed() {
-    let oldImage = imageNum
-    let oldText = textNum
+    imageNum = nonRepeatingRndm(oldVal: imageNum, count: 10)
+    textNum = nonRepeatingRndm(oldVal: textNum, count: 6)
+    audioNum = nonRepeatingRndm(oldVal: audioNum, count: 6)
+    playAudio(file: audioNum)
     
+  }
+  
+  func nonRepeatingRndm(oldVal: Int, count: Int) -> Int {
+    var newVal: Int
     repeat {
-      imageNum = Int.random(in: 0...9)
-    } while (imageNum == oldImage)
+      newVal = Int.random(in: 0..<count)
+    } while (newVal == oldVal)
+    return newVal
+  }
+  
+  func playAudio(file: Int) {
+    guard let soundData = NSDataAsset(name: "sound\(file)") else {
+      print("Failed to read the file.")
+      return
+    }
     
-    repeat {
-      textNum = Int.random(in: 0...5)
-    } while (textNum == oldText)
+    do {
+      audioPlayer = try AVAudioPlayer(data: soundData.data)
+      audioPlayer.play()
+    } catch {
+      print("Error while creating the audio file.")
+    }
   }
   
 }
