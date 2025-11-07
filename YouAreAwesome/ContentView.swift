4e26a5cb9828db0070ba7e1d634c990bbf5a94c3
cagataycemaksoy <cemaksoy7890@gmail.com>
2025-11-07 13:18:57 +0100

    Toggle added

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -11,9 +11,10 @@ import AVFAudio
 struct ContentView: View {
   @State private var audioPlayer: AVAudioPlayer!
   
+  @State private var audioNum = Int.random(in: 0...5)
   @State private var imageNum = Int.random(in: 0...9)
   @State private var textNum = Int.random(in: 0...5)
-  @State private var audioNum = Int.random(in: 0...5)
+  @State private var soundOn = false
   
   private let phrases = ["You Are Great!", "Great Energy!", "You Are Awesome!", "Such a kind person.", "You Are Fantastic!", "Fabulous! That is you."]
   
@@ -34,15 +35,29 @@ struct ContentView: View {
             
           Spacer()
           Spacer()
-          Button("Press Me!") {
-            withAnimation(.easeIn) {
-              buttonPressed()
+          HStack {
+            
+            Toggle("Sound On: ", isOn: $soundOn)
+              .fixedSize(horizontal: true, vertical: false)
+              .onChange(of: soundOn) {
+                if (audioPlayer != nil && audioPlayer.isPlaying) {
+                  audioPlayer.stop()
+                }
+              }
+              
+            
+            Spacer()
+            Button("Next!") {
+              withAnimation(.easeIn) {
+                buttonPressed()
+              }
             }
+            .tint(.black)
+            .buttonStyle(.bordered)
+            .font(.title3)
+            .fontWeight(.semibold)
           }
-          .tint(.black)
-          .buttonStyle(.bordered)
-          .font(.title3)
-          .fontWeight(.semibold)
+          .padding(.horizontal)
         }
         .padding()
     }
@@ -51,8 +66,9 @@ struct ContentView: View {
     imageNum = nonRepeatingRndm(oldVal: imageNum, count: 10)
     textNum = nonRepeatingRndm(oldVal: textNum, count: 6)
     audioNum = nonRepeatingRndm(oldVal: audioNum, count: 6)
-    playAudio(file: audioNum)
-    
+    if soundOn {
+      playAudio(file: audioNum)
+    }
   }
   
   func nonRepeatingRndm(oldVal: Int, count: Int) -> Int {
